2278ff465d37da0531b917f86ad4868dcceda22b
kgonchuk <123867028+kgonchuk@users.noreply.github.com>
2023-08-03 12:40:42 +0300

    statistics jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,20 @@
 import { Profile } from "./Profile/Profile";
-
+import user from "data/user.json"
+import { Statistics } from "components/Statistics/Statistics.jsx";
+import data from "data/data.json"
 export const App = () => {
   return (
-    <div>
-     <Profile/>
-    </div>
+    <>
+     <Profile 
+     username={user.username}
+    tag={user.tag}
+    location={user.location} 
+    avatar={user.avatar} 
+    stats={user.stats}
+     />
+     <Statistics 
+     title="Upload stats"
+     stats={data}/>
+    </>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,14 @@
+export const Statistics =  ({title, stats})  => {
+    return (
+    <section class="statistics">
+        {title && <h2 class="title">{title}</h2>}
+    
+  
+    <ul class="stat-list">
+        {stats.map(({id, label, percentage}) =>(<li class="item" key={id}>
+        <span class="label">{label}</span>
+        <span class="percentage">{percentage}</span>
+      </li>))}
+    </ul>
+  </section>)
+}
